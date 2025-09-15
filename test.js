96c2775f26faa840c095046bf0b7345b536c3a6a
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-09-15 20:54:12 +0900

    이터러블 중심 프로그래밍을 위한 L.range, L.map, L.filter 함수 추가 및 take 함수 구현

---
+++ test.js
@@ -0,0 +1,95 @@
+import { go, filter, map, reduce, log, add, curry, pipe } from "./fx.js";
+
+// 동작 방식이 핵심차이 (즉시평가 vs 느긋한 평가)
+
+// 호출 즉시 모든 요소를 한 번에 배열에 담아 반환
+const range = (l) => {
+  let i = -1;
+  const res = [];
+
+  while (++i < l) {
+    res.push(i);
+  }
+  return res;
+};
+
+var list = range(4);
+
+// ## 느긋한 L.range
+// 배열을 미리 만들지 않고, 값이 필요할 때마다 하나씩 생성(yield)
+// list 자체는 배열이 아니라 이터레이터 객체
+const L = {};
+L.range = function* (l) {
+  console.log("2");
+  let i = -1;
+  while (++i < l) {
+    yield i;
+  }
+};
+
+var list = L.range(4);
+
+function test(name, time, f) {
+  console.time(name);
+  while (time--) f();
+  console.timeEnd(name);
+}
+
+// test("range", 10, () => reduce(add, range(1000000)));
+// test("L.range", 10, () => reduce(add, L.range(1000000)));
+
+// take - 이터러블에서 원하는 길이만큼의 값을 가져오는 함수
+const take = curry((l, iter) => {
+  console.log("1");
+  let res = [];
+  iter = iter[Symbol.iterator]();
+  let cur;
+  while (!(cur = iter.next()).done) {
+    const a = cur.value;
+    res.push(a);
+    if (res.length === l) return res;
+  }
+  return res;
+});
+
+// 이터러블 중심 프로그래밍에서의 지연 평가 (Lazy Evaluation)
+// - 제때 계산법
+// - 느긋한 계산법
+// - 제너레이터이터레이터 프로토콜을 기반으로 구현
+
+// L.map
+L.map = curry(function* (fn, iter) {
+  iter = iter[Symbol.iterator]();
+  let cur;
+  while (!(cur = iter.next()).done) {
+    const a = cur.value;
+    yield fn(a);
+  }
+  // for (const a of iter) yield fn(a);
+});
+
+// L.filter
+L.filter = curry(function* (fn, iter) {
+  iter = iter[Symbol.iterator]();
+  let cur;
+  while (!(cur = iter.next()).done) {
+    const a = cur.value;
+    if (fn(a)) yield a;
+  }
+});
+
+L.entries = function* (obj) {
+  for (const k in obj) yield [k, obj[k]];
+};
+
+const join = curry((sep = ",", iter) =>
+  reduce((a, b) => `${a}${sep}${b}`, iter)
+);
+
+const queryStr = pipe(
+  L.entries,
+  L.map(([k, v]) => `${k}=${v}`),
+  join("&")
+);
+
+log(queryStr({ limit: 10, offset: 20, type: "notice" })); // limit=10&offset=20&type=notice
