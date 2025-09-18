615ba38ecbbd4111ebef0cc38ac7fd6b7ff22953
GanziMan <qjatn50089@gmail.com>
2025-09-19 00:08:44 +0900

    docs: 함수형 프로그래밍 정의 다듬기

--- test.js
+++ test.js
@@ -40,7 +40,6 @@ function test(name, time, f) {
 
 // take - 이터러블에서 원하는 길이만큼의 값을 가져오는 함수
 const take = curry((l, iter) => {
-  console.log("1");
   let res = [];
   iter = iter[Symbol.iterator]();
   let cur;
@@ -93,3 +92,65 @@ const queryStr = pipe(
 );
 
 log(queryStr({ limit: 10, offset: 20, type: "notice" })); // limit=10&offset=20&type=notice
+
+// 이터러블 중심 프로그래밍 실무적인 코드
+L.flatten = function* (iter) {
+  for (const a of iter) {
+    if (a && a[Symbol.iterator]) {
+      for (const b of a) yield b;
+    } else yield a;
+  }
+};
+
+const users = [
+  {
+    name: "a",
+    age: 21,
+    family: [
+      { name: "a1", age: 53 },
+      { name: "a2", age: 51 },
+      { name: "a3", age: 50 },
+      { name: "a4", age: 49 },
+    ],
+  },
+  {
+    name: "b",
+    age: 22,
+    family: [
+      { name: "b1", age: 54 },
+      { name: "b2", age: 52 },
+      { name: "b3", age: 48 },
+      { name: "b4", age: 46 },
+    ],
+  },
+  {
+    name: "c",
+    age: 23,
+    family: [
+      { name: "c1", age: 55 },
+      { name: "c2", age: 53 },
+    ],
+  },
+  {
+    name: "d",
+    age: 24,
+    family: [
+      { name: "d1", age: 57 },
+      { name: "d2", age: 54 },
+      { name: "d3", age: 52 },
+      { name: "d4", age: 48 },
+    ],
+  },
+];
+
+const takeAll = take(Infinity);
+go(
+  users,
+  L.map((u) => u.family),
+  L.flatten,
+
+  L.filter((u) => u.age < 50),
+  L.map((u) => u.name),
+  takeAll,
+  log
+);
